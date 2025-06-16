9b03f519c41871d6166f36141d06bbc4ff7e2c9c
spoyri <saku.poyri@gmail.com>
2025-06-16 13:16:11 +0300

    allowed LLM to call functions

--- main.py
+++ main.py
@@ -33,17 +33,46 @@ def main():
     generate_content(client, messages, verbose)
 
 
-def generate_content(client, messages, verbose):
+def generate_content(client : genai.Client, messages, verbose):
+
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        )
     )
+
+    
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    if len(response.function_calls) > 0:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else: 
+        print("Response:")
+        print(response.text)
 
 
 if __name__ == "__main__":
